d257e6c46d4c585867a48f495111057a65ce8c6d
labinfo <labinfo4@secondarieclago.it>
2019-11-23 12:07:57 +0100

    Creati tutti i metodi

--- EquazioniLibrary/Equazioni.cs
+++ EquazioniLibrary/Equazioni.cs
@@ -4,6 +4,54 @@ namespace EquazioniLibrary
 {
     public class Equazioni
     {
+        public static bool IsDetermined(double a)
+        {
+            if (a != 0)
+            {
+                return true;
+            }
+            else
+                return false;
+
+        }
+        public static bool IsInconsisted(double a)
+        {
+            if (a == 0)
+            {
+                return true;
+            } else
+                return false;
+        }
+        public static bool IsDegree2(double a, double b)
+        {
+            if (a != 0)
+            {
+                return true;
+            }
+            else
+                return false;
+
+
+        }
+        public static double Delta(double a , double b, double c)
+        {
+            return (b*b)-4*a*c;
+
+        }
+        public static string EquationDegree1(double a ,double b)
+        {
+            if (a == 0 || b == 0)
+            {
+                return "Indeterminato";
+            }
+            if (a != 0 || b == 0)
+            {
+                return "Impossibile";
+            }
+            double risultato = -b / a;
+            
+            return risultato.ToString(); 
+        }
 
 
     }
