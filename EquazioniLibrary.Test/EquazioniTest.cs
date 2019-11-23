c1c34a219e5f5b071438fd3217454d53ea980b72
labinfo <labinfo4@secondarieclago.it>
2019-11-23 11:41:56 +0100

    Aggiungere i file di progetto.

---
+++ EquazioniLibrary.Test/EquazioniTest.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EquazioniLibrary.Test
+{
+    [TestClass]
+    public class EquazioniTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
